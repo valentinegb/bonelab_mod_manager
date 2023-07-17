97a67cbb3b7b6e1ae85a8c96ebc56fe8d3b77612
Valentine Briese <valentinegb@icloud.com>
2023-07-17 15:40:28 -0700

    Add `dialoguer` dependency

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@
 // - install mods in appropriate directory for PC, push using ADB for Quest
 // - uninstall mods if unsubscribed
 
-use std::{env, io::stdin, path::PathBuf};
+use std::{env, path::PathBuf};
 
-use modio::{Credentials, Modio};
+use dialoguer::{theme::ColorfulTheme, Input, Password, Select};
+use modio::{filter::In, mods, Modio};
 use serde::{Deserialize, Serialize};
 use tokio::{
     fs::{self, create_dir_all},
@@ -18,6 +19,14 @@ const BONELAB_GAME_ID: u32 = 3809;
 #[derive(Serialize, Deserialize)]
 struct AppData {
     modio_token: Option<String>,
+    installed_mods: Vec<InstalledMod>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct InstalledMod {
+    id: u32,
+    folder: (String, String),
+    version: String,
 }
 
 #[cfg(target_os = "macos")]
@@ -50,7 +59,11 @@ async fn read_app_data() -> AppData {
     {
         fs::write(
             &app_data_file_path,
-            postcard::to_vec::<_, 32>(&AppData { modio_token: None }).unwrap(),
+            postcard::to_vec::<_, 2048>(&AppData {
+                modio_token: None,
+                installed_mods: Vec::new(),
+            })
+            .unwrap(),
         )
         .await
         .unwrap();
@@ -60,36 +73,99 @@ async fn read_app_data() -> AppData {
     postcard::from_bytes(&app_data_bytes.unwrap()).unwrap()
 }
 
+async fn write_app_data(app_data: &AppData) {
+    let app_data_path = app_data_path();
+
+    create_dir_all(&app_data_path).await.unwrap();
+
+    let app_data_file_path = app_data_path.join("data");
+
+    fs::write(
+        &app_data_file_path,
+        postcard::to_vec::<_, 2048>(app_data).unwrap(),
+    )
+    .await
+    .unwrap();
+}
+
 #[tokio::main]
 async fn main() {
-    let mut modio = Modio::new(Credentials::new(env!("MODIO_API_KEY"))).unwrap();
+    let mut modio = Modio::new(env!("MODIO_API_KEY")).unwrap();
+    let app_data = read_app_data().await;
 
-    match read_app_data().await.modio_token {
+    match app_data.modio_token {
         Some(token) => {
             modio = modio.with_credentials(token);
         }
         None => {
-            println!("You are not signed in. Please enter your mod.io email:");
+            println!("You are not signed in");
+            let authentication_choice = Select::with_theme(&ColorfulTheme::default())
+                .with_prompt(
+                    "Would you like to receive a code via email or enter a manually created token?",
+                )
+                .item("Email me a code")
+                .item("Enter a token")
+                .interact()
+                .unwrap();
 
-            let mut email = String::new();
+            match authentication_choice {
+                0 => {
+                    let email: String = Input::with_theme(&ColorfulTheme::default())
+                        .with_prompt("Please enter your email")
+                        .interact_text()
+                        .unwrap();
 
-            stdin().read_line(&mut email).unwrap();
-            modio
-                .auth()
-                .request_code(&email.trim())
-                .await
-                .expect("failed to request code");
-            println!("Enter the code emailed to you:");
+                    modio
+                        .auth()
+                        .request_code(&email.trim())
+                        .await
+                        .expect("failed to request code");
 
-            let mut code = String::new();
+                    let code: String = Password::with_theme(&ColorfulTheme::default())
+                        .with_prompt("Enter the code emailed to you")
+                        .interact()
+                        .unwrap();
+                    let credentials = modio
+                        .auth()
+                        .security_code(&code.trim())
+                        .await
+                        .expect("failed to get access token");
 
-            stdin().read_line(&mut code).unwrap();
+                    modio = modio.with_credentials(credentials.clone());
 
-            let token = modio.auth().security_code(&code.trim()).await.unwrap();
+                    write_app_data(&AppData {
+                        modio_token: Some(credentials.token.unwrap().value),
+                        ..app_data
+                    })
+                    .await;
+                }
+                1 => {
+                    let token: String = Password::with_theme(&ColorfulTheme::default())
+                        .with_prompt("Please enter your token")
+                        .interact()
+                        .unwrap();
 
-            modio = modio.with_credentials(token);
+                    modio = modio.with_credentials(token.clone());
+
+                    write_app_data(&AppData {
+                        modio_token: Some(token),
+                        ..app_data
+                    })
+                    .await;
+                }
+                _ => unreachable!(),
+            }
         }
     }
 
-    let game = modio.game(BONELAB_GAME_ID);
+    let subscriptions = modio
+        .user()
+        .subscriptions(mods::filters::GameId::_in(BONELAB_GAME_ID))
+        .collect()
+        .await
+        .expect("failed to get subscriptions");
+
+    for modio_mod in subscriptions {
+        dbg!(modio_mod);
+    }
 }
