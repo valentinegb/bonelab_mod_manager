8e60aaae89b224edab0c4c646f65f92c290541c2
Valentine Briese <valentinegb@icloud.com>
2023-07-22 15:33:56 -0700

    More error handling, prepare for mod checking

--- src/app_data.rs
+++ src/app_data.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     env::{self, VarError},
     path::PathBuf,
 };
@@ -7,9 +8,26 @@ use anyhow::Result;
 use serde::{Deserialize, Serialize};
 use tokio::fs;
 
-#[derive(Serialize, Deserialize, Default)]
+#[derive(Serialize, Deserialize)]
 pub(crate) struct AppData {
     pub(crate) modio_token: Option<String>,
+    pub(crate) installed_mods: HashMap<u32, InstalledMod>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct InstalledMod {
+    pub(crate) version: String,
+    pub(crate) folder: String,
+}
+
+// PLEASE MAKE EXTRA EXTRA SURE THAT YOU DELETE THIS AND DELETE THE TOKEN BEFORE PUBLISHING
+impl Default for AppData {
+    fn default() -> Self {
+        Self {
+            modio_token: Some("eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiUlNBLU9BRVAiLCJjdHkiOiJKV1QiLCJ6aXAiOiJERUYiLCJ4NXQiOiJzR0pJLUJhTmhlTDctUjBMejJFdlhhNlQweGs9In0.lyHkLbzIvRop1xLTxdC8fviGnxVSPG_prx5Vs8vREXRO7XCj9jBRgLUGhQ22qlGMeiLv48b7dYGcFFBvX3kxWAK27uI7lt9yoJ378emSFzUDfLhrk5HMg9JvZQoZN9-1A92bHh3s7wOfXG_DGk-alXwEkkGGPsEMtcXcxop3-IQ9XrW65JqgeHK_SOfoaS8j6VCI4SkixcUXz7OBES2QvGiBfjLxeKbcp9CShu4yUIiKVBv2klYWk4O3N4hHlvBHKJuM_iE8HF7fPtWl8ovdiusvWNwwodB2idOlSpAYgZJp2QIwahUDFOK7GscDrfc2cr7OXD5DfGz_FdbNxO2U-Q.I2ioVFHrLMgeDQPGq0AX-A.h_e-1b5LqhAPFJ5kd_AEq9Mf766QuqpMw4DW4SIrBpdrNU6MyFv8PWi_FzUe5qh97y0_PXra15QGVo1J0hfXft2FJ7OXJkGJjYje7JEKUmf9wwXAQPD1CyVikxBwQ8U4kYy2bnJ7PnZh4nwPiLvLZ5ywN8zFj143Ia4Z6mSUYdHelXSPAUG_NBrtnBLujecPxduKQ4M78X4R2ssuWBVFVnLLCSmkfupMiOb4XOVuoWi5GWcjv_-29DW_jKbC3-FhBaTW5kBjhaD1kWmXMRsygZFY6HY1nte1Tb3vGeI1e903wTRWJR_7lqpK5hagkSv7uX5xvMrkIkcUol2afHbsduvOshWIjRiu0lbhv6K-53hlKHI1lEcuk7M4YOV344A_Stjp2Q4uQos4ZZfjY1bDqVAuxsPKFl_Sa9jNPl2nfm08P5hA1KRK0uK-0LLBmFym4UZs806ho1uIEm6XDV7iONTDie5ZnoL3aFqO0s0JA6ZUM-nfsdJ8LEPFyc21XEaPvOaEJGe7SrQFC34wclLsUc86-55KIH9DT7NLLIHtJa2N8CS5GjQ8OR2rV6KYabk3Zz2BNnUJqVH7wqmWaRwhti6HEI7THWLm6nFXJl29tLTfScAx6-WHLLStJDMtp-ek83zzV8Si9RhFzTnPAb7b3tEaw0cARjISpoRY7RbStVL1PtiJaHM3K7qhBiO9brF9XBCQAYkFe1NfHbWrQYE1RZ6lXmybG7ARXRjCXKwRR4UtRdtkGbWTQEMmJTD9azS5sKkhOq6ToBIgKsz4ur1E-Yg_urSbZ9mB_ObakY6Pfg11bkfZjHY8KrD0kS1Kdi1CLRk78T1_Fg8E98l6PV0nEe1LmKvKytinguyMwpDzzp2r3lTGxB4k0c6wK2fUNMC2yZUu26KhQm1YUUXE1PXIOq9KVapibuBouegFCTMT-A7jKUibxxk8OYkYkeGxaB6q2UGmD-iYG4Xr2VzebEF5LQ9G32Pp6tPBE9YcbxwBCDZpwPpPrkzv1hBpGdzgf1DSd2AawNzlk597N9Iw_LPT8A.YOAO5Gr0o9fgKbdIn7Tjow".to_string()),
+            installed_mods: HashMap::default(),
+        }
+    }
 }
 
 impl AppData {
@@ -25,18 +43,31 @@ impl AppData {
         Ok(Self::dir_path()?.join("app_data"))
     }
 
+    async fn write_default() -> Result<Self> {
+        let default = Self::default();
+
+        default.write().await?;
+
+        Ok(default)
+    }
+
     pub(crate) async fn read() -> Result<Self> {
         let path = Self::path()?;
 
         if !fs::try_exists(&path).await? {
-            let default = Self::default();
-
-            default.write().await?;
-
-            return Ok(default);
+            return Ok(Self::write_default().await?);
         }
 
-        Ok(postcard::from_bytes(&fs::read(path).await?)?)
+        let app_data = postcard::from_bytes(&fs::read(path).await?);
+
+        if app_data
+            .as_ref()
+            .is_err_and(|err| *err == postcard::Error::DeserializeUnexpectedEnd)
+        {
+            return Ok(Self::write_default().await?);
+        }
+
+        Ok(app_data?)
     }
 
     pub(crate) async fn write(&self) -> Result<()> {
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,24 @@ mod app_data;
 mod authentication;
 
 use anyhow::Result;
+use app_data::AppData;
 use authentication::authenticate;
 use console::style;
+use modio::{filter::In, mods};
+
+const BONELAB_GAME_ID: u32 = 3809;
 
 async fn try_main() -> Result<()> {
     // authenticate with mod.io
     let modio = authenticate().await?;
 
-    println!("Successfully authenticated");
-
     // get subscribed mods
+    let subscriptions = modio
+        .user()
+        .subscriptions(mods::filters::GameId::_in(BONELAB_GAME_ID))
+        .collect()
+        .await?;
+
     // spawn a task for each mod
     // for each installed mod, check if it is subscribed
     // if not subscribed, remove
@@ -26,6 +34,25 @@ async fn main() {
             "{}",
             style("Completed without any unrecoverable errors!").green()
         ),
-        Err(err) => eprintln!("{}: {err:#}", style("Error").red()),
+        Err(err) => {
+            if let Some(err) = err.downcast_ref::<modio::Error>() {
+                if err.is_auth() {
+                    if let Ok(mut app_data) = AppData::read().await {
+                        app_data.modio_token = None;
+
+                        if let Ok(_) = app_data.write().await {
+                            eprintln!(
+                                "{}: Authentication failed, you have been signed out",
+                                style("Error").red()
+                            );
+
+                            return;
+                        }
+                    }
+                }
+            }
+
+            eprintln!("{}: {err:#}", style("Error").red());
+        }
     }
 }
