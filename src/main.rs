afaf439a815cd54588256758a00f65b57eefd39d
Valentine Briese <valentinegb@icloud.com>
2023-07-22 13:16:29 -0700

    Setup basic error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    todo!();
+use anyhow::Result;
+use console::style;
+
+async fn try_main() -> Result<()> {
+    // authenticate with mod.io
+    // get subscribed mods
+    // spawn a task for each mod
+    // for each installed mod, check if it is subscribed
+    // if not subscribed, remove
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    match try_main().await {
+        Ok(_) => println!(
+            "{}",
+            style("Completed without any unrecoverable errors!").green()
+        ),
+        Err(err) => eprintln!("{}: {err:#}", style("Error").red()),
+    }
 }
